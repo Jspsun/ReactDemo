9efd9f980261992c44045e05dd3985a9febb705a
Jonathan Sun <jonathansun@squareup.com>
2018-04-17 16:15:52 -0400

    update styled demo

--- src/components/StyledComponent.jsx
+++ src/components/StyledComponent.jsx
@@ -4,6 +4,7 @@ import Styled from 'styled-components';
 export default function StyledComponent() {
   const ColoredDiv = Styled.div`
     color: red;
+    font-size: ${10 + 30}px;
   `;
   return (
     <ColoredDiv>This is a styled component</ColoredDiv>
